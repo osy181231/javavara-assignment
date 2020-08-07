3d01b348d2f01ac0ad7054edf674a7a5eddc866a
osy181231 <osy181231@gmail.com>
2020-08-07 22:41:28 +0900

    assignment
    과제입니당

---
+++ 4th week/classes.py
@@ -0,0 +1,57 @@
+import math
+
+class Complex(object):
+    def __init__(self, real, imaginary):
+        self.real = real
+        self.imaginary = imaginary
+        
+    def __add__(self, no):
+        real = self.real + no.real
+        imaginary = self.imaginary + no.imaginary
+        return Complex(real, imaginary)
+        
+    def __sub__(self, no):
+        real = self.real - no.real
+        imaginary = self.imaginary - no.imaginary
+        return Complex(real, imaginary)
+        
+    def __mul__(self, no):
+        real = self.real * no.real - self.imaginary * no.imaginary
+        imaginary = self.real * no.imaginary + self.imaginary * no.real
+        return Complex(real, imaginary)
+
+    def __truediv__(self, no):
+        denomin = no.real ** 2 + no.imaginary ** 2
+        real = (self.real * no.real + self.imaginary * no.imaginary) / denomin
+        imaginary = (self.imaginary * no.real - self.real * no.imaginary) / denomin
+        return Complex(real, imaginary)
+
+    def mod(self):
+        real = math.sqrt(self.real ** 2 + self.imaginary ** 2)
+        return Complex(real, 0)
+
+    def __str__(self):
+        if self.imaginary == 0:
+            result = "%.2f+0.00i" % (self.real)
+        elif self.real == 0:
+            if self.imaginary >= 0:
+                result = "0.00+%.2fi" % (self.imaginary)
+            else:
+                result = "0.00-%.2fi" % (abs(self.imaginary))
+        elif self.imaginary > 0:
+            result = "%.2f+%.2fi" % (self.real, self.imaginary)
+        else:
+            result = "%.2f-%.2fi" % (self.real, abs(self.imaginary))
+        return result
+
+if __name__ == '__main__':
+    c = map(float, input().split())
+    d = map(float, input().split())
+    x = Complex(*c)
+    y = Complex(*d)
+    print(*map(str, [x+y, x-y, x*y, x/y, x.mod(), y.mod()]), sep='\n')
+
+
+#성공
+#복소수 계산 원리를 사용해서 풀기는 했는데, 문제에서 기본적으로 주어진 부분의 코드를 저에게 쓰라고 하면 못할 것 같습니다ㅠ
+#다음 시간까지 Class에 대해 더 공부해서 오겠습니당!
